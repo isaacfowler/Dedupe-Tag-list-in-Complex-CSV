7d5c4799a213788a4559e7f9ecb2a76ca80c38ab
Isaac <isaacfowler@gmail.com>
2025-08-25 14:44:18 -0400

    Update code.py
    Updated to remove unnecessary comments and add some helpful comments for later

--- code.py
+++ code.py
@@ -1,8 +1,10 @@
 import csv
 
+# Configuration
 filename = 'source.csv' # CSV including entire export
 col_index = 5 # Column that includes all tags
-target_cell_list = None # Cell containing a comma separated list
+
+target_cell_list = None # Empty list used in code to contain the target cell contents
 tag_list = [] # Empty list to contain deduped tag list
 
 with open(filename, 'r') as file:
@@ -17,5 +19,4 @@ tag_list.sort()
 print(tag_list)
 with open('output.csv', 'w', newline='') as file:
     writer = csv.writer(file)
-    # for item in tag_list:
     writer.writerows([tag_list])
