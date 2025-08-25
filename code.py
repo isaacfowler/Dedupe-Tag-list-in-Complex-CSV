328163cb8b150c24b94203ff262def1e9af0663c
Isaac <isaacfowler@gmail.com>
2025-08-25 14:40:44 -0400

    Add files via upload

---
+++ code.py
@@ -0,0 +1,21 @@
+import csv
+
+filename = 'source.csv' # CSV including entire export
+col_index = 5 # Column that includes all tags
+target_cell_list = None # Cell containing a comma separated list
+tag_list = [] # Empty list to contain deduped tag list
+
+with open(filename, 'r') as file:
+    reader = csv.reader(file)
+    for i, row in enumerate(reader):
+        target_cell_list = row[col_index].split(',')
+        for item in target_cell_list:
+            if item not in tag_list:
+                tag_list.append(item)
+
+tag_list.sort()
+print(tag_list)
+with open('output.csv', 'w', newline='') as file:
+    writer = csv.writer(file)
+    # for item in tag_list:
+    writer.writerows([tag_list])
